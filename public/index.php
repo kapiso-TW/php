6c1e3ba48486566e82edd3e4aaa4e478984dfcef
kapiso-TW <000617jason@gmail.com>
2024-11-28 00:29:18 +0800

    Logout button style add

--- public/index.php
+++ public/index.php
@@ -148,7 +148,16 @@ $page = $_SESSION['page']; // ensure page correct
             </div>
             <div class="container">
                 <form method="POST" action="logout.php" onclick="">
-                    <button type="submit">Logout</button>
+                    <button class="logout" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
+                        </svg>
+                        <div class="logout-text">
+                            Button
+                        </div>
+                    </button>
                 </form>
             </div>
         </div>
